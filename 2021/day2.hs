62a6e7bb2f691d6513dd248b08869f8f34f6d63b
Eric Rykwalder <e.rykwalder@gmail.com>
2021-12-02 22:45:33 -0500

    2021 day 2

---
+++ 2021/day2.hs
@@ -0,0 +1,27 @@
+import System.IO
+    ( hClose, openFile, hGetContents, IOMode(ReadMode) )
+
+moveSub :: Num a => (a, a) -> ([Char], a) -> (a, a)
+moveSub (x, y) ("forward", qty) = (x + qty, y)
+moveSub (x, y) ("down", qty) = (x, y + qty)
+moveSub (x, y) ("up", qty) = (x, y - qty)
+moveSub (_, _) (_, _) = error "bad direction"
+
+aimSub :: Num a => (a, a, a) -> ([Char], a) -> (a, a, a)
+aimSub (x, y, aim) ("forward", qty) = (x + qty, y + (aim * qty), aim)
+aimSub (x, y, aim) ("down", qty) = (x, y, aim + qty)
+aimSub (x, y, aim) ("up", qty) = (x, y, aim - qty)
+aimSub (_, _, _) (_, _) = error "bad direction"
+  
+main :: IO ()
+main = do  
+    handle <- openFile "./input2.txt" ReadMode
+    content <- hGetContents handle
+    let directions = map ((\dir -> (head dir, read (last dir))) . words) $ lines content
+    let pos1 = foldl moveSub (0, 0) directions
+    let pos2 = foldl aimSub (0, 0, 0) directions
+    putStr (show pos1 ++ "\n")
+    let (x, y) = pos1 in putStr (show (x * y) ++ "\n")
+    putStr (show pos2 ++ "\n")
+    let (x, y, _) = pos2 in putStr (show (x * y) ++ "\n")
+    hClose handle
